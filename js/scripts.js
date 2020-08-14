50c1c00e10336028a641ae57eeafb6040f165e30
Mickael Ancel <contactperso.ma@gmail.com>
2020-08-14 17:51:23 +0200

    Build de l'app

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: "Coucou"
+        todos : [
+            {text : 'Faire une app to do list', number : 0},
+            {text : 'Améliorer cette app', number : 1},
+            {text : 'Passer au projet suivant', number : 2}
+        ]
+    },
+    methods : {
+        deleteTodo: function () {
+            console.log("todo supprimé")
+        }
     }
 })
